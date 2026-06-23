f8e3e84eaf1d353e86467543b866b3257528273f
markvncs <vmarcu090@gmail.com>
2026-06-23 19:18:06 -0300

    feat: add pir interruption configurations

--- iluminacao-assistida/drivers pir/pir.c
+++ iluminacao-assistida/drivers pir/pir.c
@@ -3,4 +3,7 @@
 
 void pirInit() {
     P1DIR &= ~BIT3;
+    P1IES &= ~BIT3;   //ativa interrupção na borda de subida
+    P1IFG &= ~BIT3;
+    P1IE  |=  BIT3;   //habilita interrupcao local do pino 1.3
 }
