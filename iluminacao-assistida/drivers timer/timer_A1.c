35f27506200db2a5e3c002f7d363fbded4b2d303
markvncs <vmarcu090@gmail.com>
2026-06-24 15:33:19 -0300

    feat: setting up timer a1.1 for reset/set + duty cycle starting at 0%

--- iluminacao-assistida/drivers timer/timer_A1.c
+++ iluminacao-assistida/drivers timer/timer_A1.c
@@ -2,7 +2,14 @@
 #include "timer_A1.h"
 
 void timerA1Init(void) {
-    TA1CTL = TASSEL__SMCLK | MC__UP | TACLR;            // sera utilizada uma frequencia de 1KHz
+    P2DIR |= BIT0;                                      //p2.0 definido como saída
+    P2SEL |= BIT0;                                      //p2.0 é saida do sinal do timer A1.1
+
+    TA1CTL = TASSEL__SMCLK | MC__UP | TACLR;            //sera utilizada uma frequencia de 1KHz ; CCR0 = SMCLK / 1000 = 1000 - 1
     
     TA1CCR0 = 1000 - 1;
+
+    TA1CCTL1 = OUTMOD_7;                                //reset em ccr1 e set em ccr0
+
+    TA1CCR1 = 0;
 }
